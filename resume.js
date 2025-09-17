f6916c96199b10d558527d56e3432c06f014b965
N1M1237 <ojonimiedime@gmail.com>
2025-09-17 09:18:34 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,12 @@
+// Define Variables
+let myName = "Oj";
+let title = "Student";
+let experience = "Peer Tutoring, Internship, Code class";
+let education = "Oral Roberts University";
+let skills = ["Skateboarding", "Swimming", "Coking", "Programming", "Running" ];
+
+//
+document.getElementById("my-name").textContent = myName;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
